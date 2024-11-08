adc39009e9b4bc0ec84ad0f18623dc30ef6a5548
php8database <php8database@gmail.com>
2024-11-08 16:08:18 +0800

    使用PDO連線資料庫,並以HTML將資料顯示在網頁上

--- pdo.php
+++ pdo.php
@@ -10,20 +10,52 @@
 <?php
 $dsn= "mysql:host=localhost;charset=utf8;dbname=school";
 $pdo=new PDO($dsn,'root','');
-
 $sql="select * from classes";
-
 $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+?>
+<style>
+    table{
+        border-collapse: collapse;
+        width: 400px;
+        margin: auto;
+    }
+
+    table tr:nth-child(1) td{
+        background-color: #cc6;
+        color:darkorange;
+        text-shadow: 2px 2px 2px #fff;
+    }
+    table td{
+        padding:5px 15px;
+        text-align: center;
+        border:1px solid #ccc;
+    }
+</style>
+<table>
+    <tr>
+        <td>序號</td>
+        <td>班級名稱</td>
+        <td>班導師</td>
+    </tr>
+<?php
 
 foreach($rows as $row){
-    echo $row['id']."-".$row['name']."-".$row['tutor']."<br>";
+?>
+    <tr>
+        <td><?=$row['id'];?></td>
+        <td>
+            <a href="classes_detail.php?id=<?=$row['id'];?>">
+                <?=$row['name'];?>
+            </a>
+        </td>
+        <td><?=$row['tutor'];?></td>
+    </tr>
+<?php
 }
 
-
-/* echo "<pre>";
-print_r($rows);
-echo "</pre>"; */
-
 ?>
+</table>
+
+
 </body>
 </html>
